7af2e7003536594a081e6f0b5bf38a216759f64c
Luis Correa <jluiscorrea90@outlook.com>
2018-04-15 21:07:37 -0500

    mapa tags
    pongo el mapa solo con el drag para los tags... falta darle un logueo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,11 +42,14 @@
 
             <!-- Banner -->
                 <section id="banner">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m34!1m12!1m3!1d62074.572424237434!2d-72.10099884679778!3d-13.494988409330126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m19!3e0!4m5!1s0x916dd5d8de36c0f7%3A0xe5f7fdd5fce49634!2sMunicipalidad+Distrital+de+Santiago%2C+Giraldo+Ruiz+Caro%2C+Cusco+08000!3m2!1d-13.5258369!2d-71.98301169999999!4m5!1s0x916dd6b537a7a913%3A0x76fd2221fe962f07!2sPoroy%2C+Cusco!3m2!1d-13.5019104!2d-72.0320017!4m5!1s0x916db705dfa0e11d%3A0xe789e70afeae2c98!2sProvincia+de+Anta!3m2!1d-13.462225199999999!2d-72.1494902!5e0!3m2!1ses-419!2spe!4v1523209781173" width="1920" height="470" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <!--<iframe src="https://www.google.com/maps/embed?pb=!1m34!1m12!1m3!1d62074.572424237434!2d-72.10099884679778!3d-13.494988409330126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m19!3e0!4m5!1s0x916dd5d8de36c0f7%3A0xe5f7fdd5fce49634!2sMunicipalidad+Distrital+de+Santiago%2C+Giraldo+Ruiz+Caro%2C+Cusco+08000!3m2!1d-13.5258369!2d-71.98301169999999!4m5!1s0x916dd6b537a7a913%3A0x76fd2221fe962f07!2sPoroy%2C+Cusco!3m2!1d-13.5019104!2d-72.0320017!4m5!1s0x916db705dfa0e11d%3A0xe789e70afeae2c98!2sProvincia+de+Anta!3m2!1d-13.462225199999999!2d-72.1494902!5e0!3m2!1ses-419!2spe!4v1523209781173" width="1920" height="470" frameborder="0" style="border:0" allowfullscreen></iframe>
                     <header>
                         <h2>Titulo <a href="#">con descripcion</a></em></h2>
                         <a href="#" class="button">primer link</a>
-                    </header>
+                    </header>-->
+                    
+
+
                 </section>
 
             <!-- Highlights -->
@@ -228,4 +231,97 @@
             <script src="assets/js/util.js"></script>
             <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
             <script src="assets/js/main.js"></script>
+
+            <script>
+                    function initMap(){
+                      // Map options
+                      var options = {
+                        zoom:8,
+                        center:{lat:-13.5341804,lng:-71.9355579}
+                      }
+
+                      // New map
+                      var map = new google.maps.Map(document.getElementById('banner'), options);
+
+                      // Listen for click on map
+                      google.maps.event.addListener(map, 'click', function(event){
+                        // Add marker
+                        addMarker({coords:event.latLng});
+                      });
+
+                      /*
+                      // Add marker
+                      var marker = new google.maps.Marker({
+                        position:{lat:42.4668,lng:-70.9495},
+                        map:map,
+                        icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+                      });
+
+                      var infoWindow = new google.maps.InfoWindow({
+                        content:'<h1>Lynn MA</h1>'
+                      });
+
+                      marker.addListener('click', function(){
+                        infoWindow.open(map, marker);
+                      });
+                      */
+
+                      // Array of markers
+                      var markers = [
+                        {
+                          coords:{lat:-13.5341804,lng:-71.9355579},
+                          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                          content:'<h1>Cusco</h1>'
+                        },
+                        {
+                          coords:{lat:-13.4985056,lng:-72.5245806},
+                          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                          content:'<h1>Anta</h1>'
+                        },
+                        /*{
+                          coords:{lat:-13.5341804,lng:-71.9355579},
+                          content:'<h1>Cusco</h1>'
+                        },*/
+                        {
+                          coords:{lat:-13.4985056,lng:-72.5245806}
+                        }
+                      ];
+
+                      // Loop through markers
+                      for(var i = 0;i < markers.length;i++){
+                        // Add marker
+                        addMarker(markers[i]);
+                      }
+
+                      // Add Marker Function
+                      function addMarker(props){
+                        var marker = new google.maps.Marker({
+                          position:props.coords,
+                          map:map,
+                          //icon:props.iconImage
+                        });
+
+                        // Check for customicon
+                        if(props.iconImage){
+                          // Set icon image
+                          marker.setIcon(props.iconImage);
+                        }
+
+                        // Check content
+                        if(props.content){
+                          var infoWindow = new google.maps.InfoWindow({
+                            content:props.content
+                          });
+
+                          marker.addListener('click', function(){
+                            infoWindow.open(map, marker);
+                          });
+                        }
+                      }
+                    }
+                  </script>
+                  <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIm1q4xe1K1u4Aq9HEd-pm0z7aikJT5ec&callback=initMap">
+    </script>
+
 @endsection
